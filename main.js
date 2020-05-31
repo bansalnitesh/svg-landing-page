15016d30423b0d44e6aef29396b7533147475915
Nitesh <nitesh.bansal@dcmail.ca>
2020-05-31 16:23:34 -0400

    added the stuff to git dir

---
+++ main.js
@@ -0,0 +1,134 @@
+let delayTime = .3;
+
+TweenMax.fromTo('#num0', 0.6, {
+    x: -100,
+    y: -200,
+    opacity: 0
+},{
+    x: 50,
+    y: -120,
+    opacity: 0.7
+});
+TweenMax.fromTo('#num0', 0.6, {
+    x: 30,
+    y: -140,
+    opacity: 0.7
+},{
+    x: 0,
+    y:0,
+    opacity: 1,
+    delay: delayTime
+});
+TweenMax.fromTo('#num1', 0.6, {
+    x: -100,
+    y: -200,
+    opacity: 0
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+});
+TweenMax.fromTo('#num2', 0.6, {
+    x: -200,
+    y: -300,
+    opacity: 0
+},{
+    x: 0,
+    y: -100,
+    opacity: 0.6,
+});
+TweenMax.fromTo('#num2', 0.6, {
+    x: 0,
+    y: -100,
+    opacity: 0.6
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+    delay: delayTime
+});
+TweenMax.fromTo('#num3', 0.6, {
+    x: -300,
+    y: -120,
+    opacity: 0
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+});
+TweenMax.fromTo('#num4', 0.6, {
+    x: -300,
+    y: -400,
+    opacity: 0
+},{
+    x: -10,
+    y: -100,
+    opacity: 0.2,
+});
+TweenMax.fromTo('#num4', 0.6, {
+    x: -10,
+    y: -100,
+    opacity: 0.7,
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+    delay: delayTime
+});
+TweenMax.fromTo('#num5', 0.6, {
+    x: -300,
+    y: -90,
+    opacity: 0
+},{
+    x: -80,
+    y: -40,
+    opacity: 0.7,
+});
+TweenMax.fromTo('#num5', 0.6, {
+    x: -80,
+    y: -40,
+    opacity: 0.7
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+    delay: delayTime
+});
+TweenMax.fromTo('#num6', 0.6, {
+    x: -200,
+    y: -200,
+    opacity: 0.7
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+});
+TweenMax.fromTo('#num7', 0.6, {
+    x: -300,
+    y: -100,
+    opacity: 0.7
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+});
+TweenMax.fromTo('#num8', 0.6, {
+    x: -50,
+    y: -100,
+    opacity: 0.7
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+});
+TweenMax.fromTo('#num9', 0.6, {
+    x: -100,
+    y: -50,
+    opacity: 0.7
+},{
+    x: 0,
+    y: 0,
+    opacity: 1,
+});
+
+
